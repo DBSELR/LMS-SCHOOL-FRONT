79776cb3e52057ab8163a76d3d93f67c32ed70d4
DBSELR <89074505+DBSELR@users.noreply.github.com>
2025-10-15 14:53:02 +0530

    sd

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
-const API_BASE_URL = "https://localhost:7099/api";
+const API_BASE_URL = "https://localhost:7099/api"; //https://5mapi.dbasesolutions.in/
 
 
 export default API_BASE_URL;
