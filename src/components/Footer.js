df5383d1bc9478793601c594b067593e90b4a88b
DBSELR <89074505+DBSELR@users.noreply.github.com>
2025-11-04 15:08:37 +0530

    d

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer fixed-footer">
       <div className="container-fluid">
-        <div className="row justify-content-center">
+        <div className="row justify-content-end">
           <div className="col-auto text-center">
             Powered by
             <span className="d-none d-md-inline"> - D Base Solutions Private Limited (DBS)</span>
